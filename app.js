a8c8eb236ae2fd6c8813eb6b1b92b343eb338822
airsen <jason61719@gmail.com>
2012-07-28 11:57:17 +0800

    引入socket.io

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 /**
  * Module dependencies.
  */
@@ -6,30 +5,48 @@
 var express = require('express')
   , routes = require('./routes')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , io = require('socket.io');
 
 var app = express();
 
-app.configure(function(){
+app.configure(function () {
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
+  app.set('view engine', 'html');
+  app.engine('html', require('ejs').renderFile);
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-  app.use(express.cookieParser('your secret here'));
+  app.use(express.cookieParser('air'));
   app.use(express.session());
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
 });
 
-app.configure('development', function(){
+app.configure('development', function () {
   app.use(express.errorHandler());
+});
+
+// 应用的常量，可以在模板中直接取到值
+app.locals({
+  const:{
+  }
 });
 
 app.get('/', routes.index);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+http.createServer(app).listen(app.get('port'), function () {
+  console.log("服务器已启动，监听端口号：" + app.get('port'));
+});
+
+// socket.io
+io = io.listen(app);
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello:'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
 });
